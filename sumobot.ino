12e096be65e91e41f33eb047fa4260c28e09a4e2
Tanupat Atthakorn <tanupatatthakorn@gmail.com>
2019-09-09 17:09:29 +0700

    added RC control V1

--- sumobot.ino
+++ sumobot.ino
@@ -12,9 +12,21 @@
 #define RIR A4
 //detection sensors
 
+//RC
+#define CH1 9
+#define CH2 8
+#define CH3 7
+#define CH4 6
+
+int tmpCH1;
+int tmpCH2;
+int tmpCH3;
+int tmpCH4;
 
 void setup() {
   // put your setup code here, to run once:
+
+  Serial.begin(9600);
   //switches
   pinMode(SW, INPUT);
   //motors
@@ -28,19 +40,49 @@ void setup() {
   pinMode(LIR, INPUT);
   pinMode(RIR, INPUT);
   //detection sensors
-  
 
+  //RC
+  pinMode(CH1, INPUT);
+  pinMode(CH2, INPUT);
+  pinMode(CH3, INPUT);
+  pinMode(CH4, INPUT);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(digitalRead(SW)==LOW){
-      test();
 
+  tmpCH1 = pulseIn(CH1, HIGH);
+  tmpCH2 = pulseIn(CH2, HIGH);
+  tmpCH3 = pulseIn(CH3, HIGH);
+  tmpCH4 = pulseIn(CH4, HIGH);
+
+  debug();
+
+ Controller(tmpCH1, tmpCH2, tmpCH3, tmpCH4);
+  
+  if(digitalRead(SW)==LOW){
+      //test();
+      
       STOP();
       while(1);
     }
   }
+
+void debug(){
+    Serial.print(tmpCH1);
+    Serial.print("\t");
+    Serial.print(tmpCH2);
+    Serial.print("\t");
+    Serial.print(tmpCH3);
+    Serial.print("\t");
+    Serial.print(tmpCH4);
+    Serial.print("\n");
+    
+//    Serial.println(digitalRead(CH2));
+//    Serial.println(digitalRead(CH3));
+//    Serial.println(digitalRead(CH4));
+}
 
 void test(){
       //forward(100, 100);
@@ -119,5 +161,43 @@ void rspin(int spd){
 
   analogWrite(LPWM, spd);
   analogWrite(RPWM, spd);
+  
+}
+
+void Controller(int tmpCH1, int tmpCH2, int tmpC3, int tmpCH4){
+  //for/back
+  int RU = map(tmpCH2, 1600, 1981, 0, 255);
+  int RD = map(tmpCH2, 1400, 995, 0, 255);
+  if(tmpCH2>1400 && tmpCH2<1600){
+    RU= 0;
+    RD=0;
+  }
+  if(tmpCH2==0){
+    RU = 0;
+    RD = 0;
+  }
+  //left/right
+  int LR = map(tmpCH4, 1550, 1981, 0, 255);
+  int LL = map(tmpCH4, 1450, 995, 0, 255);
+  if(tmpCH4>1400 && tmpCH4<1600){
+    LR = 0;
+    LL = 0;
+  }
+  
+  (RU<0)?RU=0:RU=RU;
+  (RD<0)?RD=0:RD=RD;
+
+  (LR<0)?LR=0:LR=LR;
+  (LL<0)?LL=0:LL=LL;
+
+  if(LR>0){
+    frspin(LR);
+  }else if(LL>0){
+    flspin(LL);
+  }else if(RU>0){
+    forward(RU, RU);
+  }else{
+    backward(RD, RD);
+  }
   
 }
